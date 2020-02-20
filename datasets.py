6ecb14aa559853f504ca582ec5f32cf7a4c8d20e
AlexanderSlav <alexander.slavutin@gmail.com>
2020-02-20 23:42:28 +0300

    esrgan source code added

---
+++ datasets.py
@@ -0,0 +1,54 @@
+import glob
+import random
+import os
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+
+# Normalization parameters for pre-trained PyTorch models
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
+
+
+def denormalize(tensors):
+    """ Denormalizes image tensors using mean and std """
+    for c in range(3):
+        tensors[:, c].mul_(std[c]).add_(mean[c])
+    return torch.clamp(tensors, 0, 255)
+
+
+class ImageDataset(Dataset):
+    def __init__(self, root, hr_shape):
+        hr_height, hr_width = hr_shape
+        # Transforms for low resolution images and high resolution images
+        self.lr_transform = transforms.Compose(
+            [
+                transforms.Resize((hr_height // 4, hr_height // 4), Image.BICUBIC),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
+        self.hr_transform = transforms.Compose(
+            [
+                transforms.Resize((hr_height, hr_height), Image.BICUBIC),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
+
+        self.files = sorted(glob.glob(root + "/*.*"))
+
+    def __getitem__(self, index):
+        print('------------',index,' / ',len(self.files),'-----------------' )
+        img = Image.open(self.files[index % len(self.files)])
+        lr_image = self.lr_transform(img)
+        hr_image = self.hr_transform(img)
+    
+
+        return {"lr": lr_image, "hr": hr_image}
+
+    def __len__(self):
+        return len(self.files)
